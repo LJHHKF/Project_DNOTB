ce16ba17cb21798649f8a5aac44662b9f0579e7c
LJHHKF <43880473+LJHHKF@users.noreply.github.com>
2021-09-08 18:51:01 +0900

    09.08_2

--- Assets/Scripts/SceneFunc/MainGame/DetectOnMouseClick.cs
+++ Assets/Scripts/SceneFunc/MainGame/DetectOnMouseClick.cs
@@ -7,8 +7,8 @@ public abstract class DetectOnMouseClick : MonoBehaviour
     [Header("For Detect On Mouse Click Setting")]
     [SerializeField] protected Transform leftUp;
     [SerializeField] protected Transform rightDown;
-    protected Vector2 v_leftUp;
-    protected Vector2 v_rightDown;
+    //protected Vector2 v_leftUp;
+    //protected Vector2 v_rightDown;
     protected Vector2 mousePosition;
     protected Camera myCam;
     protected bool isDowned;
@@ -18,8 +18,8 @@ public abstract class DetectOnMouseClick : MonoBehaviour
     {
         myCam = Camera.main;
 
-        v_leftUp = leftUp.position;
-        v_rightDown = rightDown.position;
+        //v_leftUp = leftUp.position;
+        //v_rightDown = rightDown.position;
         isDowned = false;
     }
 
@@ -31,12 +31,12 @@ public abstract class DetectOnMouseClick : MonoBehaviour
             mousePosition = Input.mousePosition;
             mousePosition = myCam.ScreenToWorldPoint(mousePosition);
 
-            Debug.Log($"Down, v_leftUp: {v_leftUp}, v_rightDown: {v_rightDown}, mousePosition: {mousePosition}");
+            Debug.Log($"Down, v_leftUp: {(Vector2)leftUp.position}, v_rightDown: {(Vector2)rightDown.position}, mousePosition: {mousePosition}");
 
-            if (mousePosition.x > v_leftUp.x
-            && mousePosition.x < v_rightDown.x
-            && mousePosition.y > v_rightDown.y
-            && mousePosition.y < v_leftUp.y)
+            if (mousePosition.x > leftUp.position.x
+            && mousePosition.x < rightDown.position.x
+            && mousePosition.y > rightDown.position.y
+            && mousePosition.y < leftUp.position.y)
                 isDowned = true;
             else
                 isDowned = false;
@@ -48,12 +48,12 @@ public abstract class DetectOnMouseClick : MonoBehaviour
                 mousePosition = Input.mousePosition;
                 mousePosition = myCam.ScreenToWorldPoint(mousePosition);
 
-                Debug.Log($"Up, v_leftUp: {v_leftUp}, v_rightDown: {v_rightDown}, mousePosition: {mousePosition}");
+                Debug.Log($"Up, v_leftUp: {(Vector2)leftUp.position}, v_rightDown: {(Vector2)rightDown.position}, mousePosition: {mousePosition}");
 
-                if (mousePosition.x > v_leftUp.x
-                && mousePosition.x < v_rightDown.x
-                && mousePosition.y > v_rightDown.y
-                && mousePosition.y < v_leftUp.y)
+                if (mousePosition.x > leftUp.position.x
+                && mousePosition.x < rightDown.position.x
+                && mousePosition.y > rightDown.position.y
+                && mousePosition.y < leftUp.position.y)
                     Execute();
             }
         }
